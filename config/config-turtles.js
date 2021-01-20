aef62002fa6e6a4bcc99e28c2512612c808b8d48
agent48co <agent48co@yandex.ru>
2021-01-20 22:19:11 +0300

    small improvements

--- config/config-turtles.js
+++ config/config-turtles.js
@@ -8,10 +8,10 @@ const configJumpoJimbo = {
             currency: 'USDT',
             asset: 'BTC'
         },
-// feb:
+
         daterange: {
-            from: '2020-02-21 00:00',
-            to: '2020-03-21 00:00'
+            from: '2018-01-01 00:00',
+            to: '2021-01-01 00:00'
         },
 
         simulationBalance: {
@@ -27,7 +27,7 @@ const configJumpoJimbo = {
     apiUrl: 'http://localhost:4000',
 
     // Population size, better reduce this for larger data
-    populationAmt: 4,
+    populationAmt: 8,
 
     // How many completely new units will be added to the population (populationAmt * variation must be a whole number!!)
     variation: 0.5,
@@ -47,17 +47,17 @@ const configJumpoJimbo = {
     // profit = recommended!
     // profitForMinSharpe = same as profit but sharpe will never be lower than minSharpe
     // profitBatched = min profitable for each month (use with batchBacktest new API) - use G as passive monthly income!
-    //mainObjective: 'profit',
-    mainObjective: 'profitBatched',
-    // mainObjective: 'profitForLimitedLossesLimitedMaxExposure',
+    // mainObjective: 'profit',
 
-    candleValues: [ 60 ],
-    // candleValues: [ 15, 30 ],
+    mainObjective: 'profitBatched',
+    BATCH_PERIOD_MIN_PROFIT: -5, // use -Infinity to disregard
+    BATCH_MAX_ALLOWED_NON_PROFIT_PERIODS: Infinity, // disregarding
+    candleValues: [ 30, 45, 60, 120, 240 ],
 
     getProperties: () => ({
 
         candleSize: randomExt.pick(configJumpoJimbo.candleValues),
-        historySize: 280, // just to match warmup 11 days for 1 hr candles
+        historySize: 50, // just to match MAX ATR
 
         enterFast: randomExt.integer(30, 10),
         exitFast: randomExt.integer(20, 1),
@@ -71,22 +71,9 @@ const configJumpoJimbo = {
 
         hiekenAshi: randomExt.pick([ true, false ]),
         margin: {
-            useShort: randomExt.pick([ true, false ])
+            //useShort: randomExt.pick([ true, false ])
+            useShort: true
         }
-        // enterFast: 20,
-        // exitFast: 10,
-        // enterSlow: 55,
-        // exitSlow: 20,
-        //
-        // useAtrStop: true,
-        // useTrailingAtrStop: true,
-        // atrPeriod: 39,
-        // atrStop: 20,
-        //
-        // hiekenAshi: false,
-        // margin: {
-        //     useShort: true
-        // }
     }),
 
     // not used:
